5a542489be9968f7b160a7ce8338969d3ebb5aca
Kalyvas Emmanouil <emkalyvas@mnapps.net>
2023-05-23 15:10:37 +0000

    update

--- src/myAD/main_my_detector.py
+++ src/myAD/main_my_detector.py
@@ -1,18 +1,24 @@
 import helper
-from mydetector import mad
+from mydetector import mad, Method
+
+# Config :
+TEST_CASE_NAME = "OneClassSVM_002"
+MODEL = Method.ONE_CLASS_SVM
+
 
 if __name__ == '__main__':
     data=helper.get_test_data()
     datatrain=helper.get_train_data()
 
-    model=mad()
+    print(f"Loading model '{MODEL}'...")
+    model=mad(MODEL)
 
+    print("Training model...")
     model.fit(datatrain["train"])
 
+    print("Model trained, generating scores and predictions...")
     data['test_scores']=model.getscores(data["test"])
+    data['test_preds']=model.getpreds(data["test"],th=0.95)
 
-    data['test_preds'] =model.getpreds(data["test"],th=0.95)
-
-
-    helper.evaluation(data,experiment_name="try_0001",scores=True)
-
+    print("Evaluating model...")
+    helper.evaluation(data,experiment_name=TEST_CASE_NAME,scores=True)
--- src/myAD/mydetector.py
+++ src/myAD/mydetector.py
@@ -2,35 +2,39 @@ import numpy as np
 from sklearn.svm import OneClassSVM
 from sklearn.ensemble import IsolationForest
 
+class Method:
+    ONE_CLASS_SVM = "OneClassSVM"
+    ISOLATION_FOREST = "IsolationForest"
+
 # example of implementation
 class mad:
 
-    def __init__(self):
-        self.model = None
-        pass
+    def __init__(self,method = Method.ONE_CLASS_SVM):
+        if method == Method.ONE_CLASS_SVM:
+            self.model = OneClassSVM(gamma='auto')
+        elif method == Method.ISOLATION_FOREST:
+            self.model = IsolationForest(n_estimators=100, max_samples='auto', contamination='auto', random_state=42)
 
     def fit(self,trainingdata) -> None:
         allData = []
         for trace in trainingdata:
             for data in trace:
                 allData.append(data)
-
-        self.model = OneClassSVM(gamma='auto').fit(allData)
+        self.model = self.model.fit(allData)
 
     def predict(self,records,threshold=0):
-        return self.model.predict(records)
+        return [ 1 if a == -1 else 0 for a in self.model.predict(records) ] # 0=Normal, 1=Anomaly
 
     def score(self,records):
-        return self.model.score_samples(records)
+        return self.model.decision_function(records)
 
 
     def getscores(self,data):
         numpy_3d_data = data
         scores2d = []
         for period in numpy_3d_data:
-            scoresforPeriod=[]
-            for record in period:
-                scoresforPeriod.append(self.score([record])[0]) #We only get the first one because we only ask for one score
+            #for record in period:
+            scoresforPeriod = self.score(period)
             scores2d.append(np.array(scoresforPeriod))
         # Remember the returned score must be in shape (#periods,#records)
         return np.array(scores2d)
@@ -39,9 +43,8 @@ class mad:
         numpy_3d_data = data
         predictions2d = []
         for period in numpy_3d_data:
-            predictionsforPeriod = []
-            for record in period:
-                predictionsforPeriod.append(self.predict([record])[0]) #We only get the first one because we only ask for one prediction
-            predictions2d.append(np.array(predictionsforPeriod))
+            #for record in period:
+            pred = self.predict(period)
+            predictions2d.append(np.array(pred))
         # Remember the returned predictions must be in shape (#periods,#records) with binary (1,0) values.
         return np.array(predictions2d)
