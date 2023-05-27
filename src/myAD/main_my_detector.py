ebd9df7c9532ecf9bb13884afe25876cad9d7288
Kalyvas Emmanouil <emkalyvas@mnapps.net>
2023-05-27 20:29:59 +0000

    Add series reconstruction technique (semi-supervised)

--- src/myAD/main_my_detector.py
+++ src/myAD/main_my_detector.py
@@ -1,10 +1,10 @@
 import helper
-from mydetector import mad, Method
+from mydetector import mad, Models
 
 # Config :
-TEST_CASE_NAME = "OneClassSVM_002"
-MODEL = Method.ONE_CLASS_SVM
-
+TEST_CASE_NAME = "OneClassSVM_0001"
+MODEL = Models.RNN
+SCORES = True
 
 if __name__ == '__main__':
     data=helper.get_test_data()
@@ -14,11 +14,13 @@ if __name__ == '__main__':
     model=mad(MODEL)
 
     print("Training model...")
-    model.fit(datatrain["train"])
+    model.fit(datatrain)
 
     print("Model trained, generating scores and predictions...")
-    data['test_scores']=model.getscores(data["test"])
-    data['test_preds']=model.getpreds(data["test"],th=0.95)
+    if SCORES:
+        data['test_scores']=model.getscores(data["test"])
+    else:
+        data['test_preds']=model.getpreds(data["test"],th=0.85)
 
     print("Evaluating model...")
-    helper.evaluation(data,experiment_name=TEST_CASE_NAME,scores=True)
+    helper.evaluation(data,experiment_name=TEST_CASE_NAME,scores=SCORES)
